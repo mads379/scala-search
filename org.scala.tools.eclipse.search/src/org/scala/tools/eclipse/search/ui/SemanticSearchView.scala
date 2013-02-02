95595fb0f48d6049bf84d57aad33cdfe129fc115
Mads Hartmann Jensen <mads379@gmail.com>
2013-02-02 13:32:22 +0100

    Added empty UI for showing search results
    Hooked it up in the plugin.xml so that it will always be visible.
    Right now I target the Java Perspective, but I'll change that
    once I figure out what the ID for the Scala perspective is.

---
+++ org.scala.tools.eclipse.search/src/org/scala/tools/eclipse/search/ui/SemanticSearchView.scala
@@ -0,0 +1,18 @@
+package org.scala.tools.eclipse.search.ui
+
+import org.eclipse.ui.part.ViewPart
+import scala.tools.eclipse.logging.HasLogger
+import org.eclipse.swt.widgets.Composite
+
+/**
+ * The main view for displaying search results
+ */
+class SemanticSearchView extends ViewPart with HasLogger {
+
+  override def createPartControl(parent: Composite) {
+    // TODO: Set up view
+  }
+
+  override def setFocus() { }
+
+}
